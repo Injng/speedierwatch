4f04a01d82d0e7cd45256f3fc82fb44c15f09c07
Pinak Paliwal <37413746+pythoncrazy@users.noreply.github.com>
2025-05-08 12:39:20 -0700

    Add pre-commit with Ruff for code formatting and linting
    diff --git a/pyproject.toml b/pyproject.toml
    index c9003cb..a7973fa 100644
    --- a/pyproject.toml
    +++ b/pyproject.toml
    @@ -8,5 +8,5 @@ dependencies = [
         "crispy-bootstrap5==2024.2",
         "django==5.0.2",
         "django-crispy-forms==2.1",
    -    "pre-commit>=4.2.0",
    +    "pre-commit>=4.2.0"
     ]

--- speedwatcher/wsgi.py
+++ speedwatcher/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'speedwatcher.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speedwatcher.settings")
 
 application = get_wsgi_application()
